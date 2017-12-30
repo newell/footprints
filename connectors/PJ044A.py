f6aa32757a907799c03d93c5279617cb12f6dd8c
Newell Jensen <newell.jensen@gmail.com>
2017-12-29 16:20:21 -0800

    Update PJ044A footprint as most DIY fab houses don't do plated slots.

--- connectors/PJ044A.py
+++ connectors/PJ044A.py
@@ -3,8 +3,8 @@
 from footgen import *
 
 f = Footgen("PJ044A", output_format='geda')
-f.add_pad("1", x=0, y=-3.7, xsize=3.5,  ysize=1.0)
-f.add_pad("2", x=0, y=2.5, xsize=3.0, ysize=1.0)
-f.add_pad("3", x=3.5, y=1.5, xsize=2.50, ysize=1.0)
+f.add_pad("1", x=0, y=-3.7, diameter=4.125, shape='circle', drill=3.5)
+f.add_pad("2", x=0, y=2.5, diameter=3.625, shape='circle', drill=3.0)
+f.add_pad("3", x=3.5, y=1.5, diameter=3.125, shape='circle', drill=2.5)
 f.silkbox(h=11.5, w=10.0) 
 f.finish()
